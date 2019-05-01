d200df16ac5c080dce7cd6e3b776b00cd1d266ac
jackowiak <matyskiewicz.j@gmail.com>
2019-05-01 14:08:53 +0200

    add tests setup

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,52 @@ import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
+import Main from './components/Main';
+import NavBar from './components/NavBar';
+import AddCharacter from './components/AddCharacter';
+
 describe('<App />', () => {
+	let appWrapper;
+	let appInstance;
+
+	const app = (disableLifecycleMethods = false) =>
+		shallow(<App />, { disableLifecycleMethods });
+
+	beforeEach(() => {
+		appWrapper = app();
+		appInstance = appWrapper.instance();
+	});
+
+	afterEach(() => {
+		appWrapper = undefined;
+		appInstance = undefined;
+	});
+
 	it('renders without crashing', () => {
-		const div = document.createElement('div');
-		ReactDOM.render(<MemoryRouter>
-			<App />
-		</MemoryRouter>, div);
-		ReactDOM.unmountComponentAtNode(div);
+		expect(app().exists()).toBe(true);
+	});
+
+	it('renders a div', () => {
+		expect(appWrapper.first().type()).toBe('div');
+	});
+
+	describe('the rendered div', () => {
+		const div = () => appWrapper.first();
+
+		it('contains everything else that gets rendered', () => {
+			expect(div().children()).toEqual(appWrapper.children());
+		});
+	});
+
+	it('renders <NavBar />', () => {
+		expect(appWrapper.find(NavBar).length).toBe(1);
+	});
+
+	describe('the rendered <NavBar />', () => {
+		const header = () => appWrapper.find(NavBar);
+
+		it('receives state.isOpen as isOpen prop', () => {
+			expect(header().prop('isOpen')).toEqual(appWrapper.state('isOpen'));
+		});
 	});
 });
